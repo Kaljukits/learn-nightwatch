0ec1ad6cdc51a36bef23d9d60d9496e521c94da7
Kaljukits <minnamiinu@outlook.com>
2019-10-24 15:24:47 +0300

    update

--- test/e2e/websitetest.js
+++ test/e2e/websitetest.js
@@ -11,15 +11,14 @@ module.exports = {
       .useXpath() // every selector now must be xpath
       .click("//div/a/span[text()='Continue to Site']")
       .pause(3000)
-      .moveTo(4,2)
-      .pause(4000)
-      .mouseButtonDown(2)
-      .pause(4000)
-      .moveTo(6,2)
+      .getLocationInView(
+        "//div/a[text()='Vol.3 Chapter 18: Lustful dragon Luslia']",
+        function(result) {}
+      )
+      .execute("scrollTo(result.value.x, result.value.y)")
       .waitForElementVisible(
         "//div/a[text()='Vol.3 Chapter 18: Lustful dragon Luslia']"
       )
-      .mouseButtonUp(2)
       .click("//div/a[text()='Vol.3 Chapter 18: Lustful dragon Luslia']")
       .waitForElementVisible("Next Page")
       .pause(3000)
