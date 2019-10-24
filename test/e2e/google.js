9de1ef4bb03b8b401a99e64e9be985c9701f20a8
Kaljukits <minnamiinu@outlook.com>
2019-10-24 13:09:30 +0300

    update

--- test/e2e/google.js
+++ test/e2e/google.js
@@ -4,12 +4,14 @@ module.exports = {
   Google: function(browser) {
     browser
       .url("https://www.google.com")
+      .pause(2500)
+      .waitForElementVisible("body")
       .waitForElementVisible("#hplogo")
       .setValue(".gLFyf.gsfi", ["cats", browser.Keys.ENTER])
       .waitForElementVisible(".q.qs")
       .assert.title("cats - Google otsing")
       .expect("body")
-      .text.to.contain("cats")
+      .assert.containsText("#main", "cats")
       .pause(2500)
       .click(".q.qs")
       .saveScreenshot(config.imgpath(browser) + "googleCat.png")
